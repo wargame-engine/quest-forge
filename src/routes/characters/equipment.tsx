0e6223afda267670a86c80753f8c57fe8b7cdfa9
Jacob Aimino <1413884+Guitarninja2@users.noreply.github.com>
2022-01-31 19:31:49 -0500

    fix

--- src/routes/characters/equipment.tsx
+++ src/routes/characters/equipment.tsx
@@ -1,13 +1,7 @@
-import { Box, Stack, TextField } from '@mui/material';
+import { Box } from '@mui/material';
 import React from 'react';
 
-type props = {
-  character: any,
-  setCharacter: Function
-};
-
-export default function Home(props: props) {
-  const { character, setCharacter } = props;
+export default function Equipment() {
   return (
     <Box sx={{ mt: 2 }}>
       Kekeroni
